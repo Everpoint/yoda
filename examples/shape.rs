de98194820908fbb9afb3a3cc419825b6dbeb8e2
maxim <maxim@gritsenko.biz>
2021-10-06 12:12:57 +0300

    Updates shape example

--- examples/shape.rs
+++ examples/shape.rs
@@ -1,17 +1,11 @@
-#[allow(unused_imports)]
-use glium::{glutin, Surface};
-
-#[macro_use]
-extern crate glium;
-
-use glium::backend::glutin::glutin::event_loop::EventLoop;
-use glium::Display;
-use glium::backend::glutin::glutin::event::ElementState;
 use yoda::map::Map;
 use yoda::layer::StaticLayer;
 use yoda::render_target::RenderTarget;
-use std::time::Duration;
-use yoda::symbol::CircleSymbol;
+use yoda::symbol::{CircleSymbol, LineSymbol, PolygonSymbol};
+use winit::event_loop::{EventLoop, ControlFlow};
+use glutin::window::Window;
+use glutin::{ContextWrapper, PossiblyCurrent};
+use glow::HasContext;
 
 fn main() {
     let shape_points = shapefile::reader::ShapeReader::from_path("./examples/data/points_wm.shp").unwrap().read().unwrap();
@@ -33,23 +27,23 @@ fn main() {
     let width = bbox[2] - bbox[0];
     let height = bbox[3] - bbox[1];
     let len = points.len();
-    for i in 0..1 {
-        for j in 0..3 {
+    for i in 0..9 {
+        for j in 0..9 {
             if i == 0 && j == 0 { continue; }
 
             for z in 0..len {
                 let p = points[z];
-                // points.push([p[0] + i as f32 * width, p[1] + j as f32 * height, 0.0]);
-                points.push([p[0], p[1], j as f32 * 30000.0]);
+                points.push([p[0] + i as f32 * width, p[1] + j as f32 * height, 0.0]);
+                // points.push([p[0], p[1], j as f32 * 30000.0]);
             }
         }
     }
 
     eprintln!("Loaded {} points. ", points.len());
 
-    let (event_loop, context) = init_window();
+    let (event_loop, window) = init_window();
 
-    let mut symbol = CircleSymbol { size: 3.0, color: [0.0, 0.7, 0.7, 1.0], program: None };
+    let mut symbol = CircleSymbol { size: 5.0, color: [0.0, 0.7, 0.7, 1.0], program: None };
     let layer = StaticLayer::new(symbol, points);
     let mut map = Map::new();
     map.set_center((bbox[2] + bbox[0]) / 2.0, (bbox[3] + bbox[1]) / 2.0);
@@ -57,6 +51,14 @@ fn main() {
 
     map.add_layer(Box::new(layer));
     let mut last_frame_time = std::time::SystemTime::now();
+
+    let gl = unsafe {
+        let gl = glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
+        gl.enable(glow::BLEND);
+        gl.blend_func(glow::SRC_ALPHA, glow::ONE_MINUS_SRC_ALPHA);
+
+        gl
+    };
 
     event_loop.run(move |event, _, control_flow| {
         match event {
@@ -66,7 +68,9 @@ fn main() {
                     return;
                 },
                 glutin::event::WindowEvent::Resized(size) => {
-                    context.gl_window().resize(size);
+                    unsafe {
+                        gl.viewport(0, 0, size.width as i32, size.height as i32);
+                    }
                     return;
                 },
                 _ => {
@@ -86,23 +90,31 @@ fn main() {
             std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-        let mut frame = context.draw();
-        frame.clear_color(0.2, 0.3, 0.3, 1.0);
+        let window_size = window.window().inner_size();
+        unsafe {
+            gl.clear_color(0.1, 0.2, 0.3, 1.0);
+            gl.clear(glow::COLOR_BUFFER_BIT);
 
-        let mut target = RenderTarget::new(&context, &mut frame);
-        map.draw(&mut target);
+            let mut target = RenderTarget::new(&gl, (window_size.width, window_size.height));
+            map.draw(&mut target);
 
-        frame.finish().unwrap();
-
-        last_frame_time = std::time::SystemTime::now();
+            window.swap_buffers();
+        }
     });
 }
 
-fn init_window() -> (EventLoop<()>, Display) {
+fn init_window() -> (EventLoop<()>, ContextWrapper<PossiblyCurrent, Window>) {
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new().with_multisampling(4);
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();;
+    let window_builder = glutin::window::WindowBuilder::new()
+        .with_title("Simple yoda map example");
+    let window = unsafe {
+        glutin::ContextBuilder::new()
+            .with_multisampling(4)
+            .build_windowed(window_builder, &event_loop)
+            .unwrap()
+            .make_current()
+            .unwrap()
+    };
 
-    (event_loop, display)
+    (event_loop, window)
 }
