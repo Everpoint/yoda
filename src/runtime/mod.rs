845a2aea024d3bb516dc1e456ef26cd816d7aa4f
Pavel <le.pashen@gmail.com>
2021-11-08 01:56:47 +0300

    Fixed lags on Windows
    * Fixed an error with swap_buffer function call on every loop iteration. Now it call only when trigger redraw event from event_loop_cycle.
    * Fix event_loop_cycle function to return redraw requested

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,13 +1,19 @@
+use std::rc::Rc;
+
+use glow::{Context, HasContext};
+use winit::event::{Event, StartCause::*, WindowEvent::*};
+use winit::event_loop::ControlFlow;
+
 use crate::map::Map;
 use crate::render_target::RenderTarget;
-use glow::{Context, HasContext};
-use std::rc::Rc;
 
 #[cfg(not(target_arch = "wasm32"))]
 pub mod native;
 
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
+
+const WAIT_TIME: instant::Duration = instant::Duration::from_nanos(16_666_667);
 
 fn event_loop_cycle(
     event: winit::event::Event<()>,
@@ -16,42 +22,42 @@ fn event_loop_cycle(
     gl: Rc<Context>,
     width: u32,
     height: u32,
-) {
+) -> bool {
+    let mut redraw_requested = false;
+
     match event {
-        winit::event::Event::WindowEvent { event, .. } => match event {
-            winit::event::WindowEvent::CloseRequested => {
-                *control_flow = winit::event_loop::ControlFlow::Exit;
-                return;
+        Event::NewEvents(cause) => match cause {
+            ResumeTimeReached { .. } => (),
+            Init => (),
+            Poll => (),
+            _ => {}
+        },
+        Event::WindowEvent { event, .. } => match event {
+            CloseRequested => {
+                *control_flow = ControlFlow::Exit;
             }
-            winit::event::WindowEvent::Resized(size) => {
-                unsafe {
-                    gl.viewport(0, 0, size.width as i32, size.height as i32);
-                }
-                return;
-            }
+            Resized(size) => unsafe {
+                gl.viewport(0, 0, size.width as i32, size.height as i32);
+            },
             _ => {
                 map.control().handle_event(&event);
-                return;
             }
         },
-        winit::event::Event::NewEvents(cause) => match cause {
-            winit::event::StartCause::ResumeTimeReached { .. } => (),
-            winit::event::StartCause::Init => (),
-            winit::event::StartCause::Poll => (),
-            _ => return,
+        Event::MainEventsCleared => unsafe {
+            gl.viewport(0, 0, width as i32, height as i32);
+            gl.clear_color(0.1, 0.2, 0.3, 1.0);
+            gl.clear(glow::COLOR_BUFFER_BIT);
+
+            let mut target = RenderTarget::new(gl, (width, height));
+            map.draw(&mut target);
+
+            redraw_requested = true;
         },
-        _ => return,
+        Event::RedrawEventsCleared => {
+            *control_flow = ControlFlow::WaitUntil(instant::Instant::now() + WAIT_TIME);
+        }
+        _ => {}
     }
 
-    let next_frame_time = instant::Instant::now() + instant::Duration::from_nanos(16_666_667);
-    *control_flow = winit::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-    unsafe {
-        gl.viewport(0, 0, width as i32, height as i32);
-        gl.clear_color(0.1, 0.2, 0.3, 1.0);
-        gl.clear(glow::COLOR_BUFFER_BIT);
-
-        let mut target = RenderTarget::new(gl, (width, height));
-        map.draw(&mut target);
-    }
+    redraw_requested
 }
